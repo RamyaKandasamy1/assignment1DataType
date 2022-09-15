21387a2831e91a6babc2b5e79df5582eb645b2f7
RamyaKandasamy1 <ramz87lakshmi@yahoo.co.in>
2022-09-14 19:47:34 -0500

    first commit

---
+++ src/assignment1DataType/differentDataTypes.java
@@ -0,0 +1,24 @@
+package assignment1DataType;
+
+
+public class differentDataTypes {
+	 public static void main(String[] args) {
+		 
+		 byte num1=10;
+		 short num2=100;
+		 int num3=1000;
+		 long num4=10000L;
+		 double num5= 10.98;
+		 float num6=0.9F;
+		 char value='A';
+		 String message="Welcome";
+
+	        boolean b1 = false;
+	       
+		 
+		 System.out.println(b1);
+	
+		 
+	
+}
+}
